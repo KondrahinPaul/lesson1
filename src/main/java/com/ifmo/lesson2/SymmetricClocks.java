2c7ca282f55dfdbcd87547e9806f6a72e90fb7f1
KondrahinPaul <sync.85@mail.ru>
2021-10-29 19:02:17 +0300

    clock

--- src/main/java/com/ifmo/lesson2/SymmetricClocks.java
+++ src/main/java/com/ifmo/lesson2/SymmetricClocks.java
@@ -13,18 +13,33 @@ public class SymmetricClocks {
     public static int symmetricTimes() {
         // TODO implement
         int count = 0;
-              for (int x = 0; x <= 1; x++) {
-                  for (int y = 0; y <= 9; y++) {
-for (int z = 0; z<=5; z++) {
-    for (int q=0;q<=9;q++) {
-        if (x==q) {
-            if (y==z) {
-                count++;
-            }
-        }
-    }
-}
-                                        }
+              for (int x = 0; x <= 2; x++) {
+                  if (x==0 && x==1) {
+                      for (int y = 0; y <= 9; y++) {
+                          for (int z = 0; z <= 5; z++) {
+                              for (int q = 0; q <= 9; q++) {
+                                  if (x == q) {
+                                      if (y == z) {
+                                          count++;
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                  }
+                  else {for (int y = 0; y <= 4; y++) {
+                      for (int z = 0; z <= 5; z++) {
+                          for (int q = 0; q <= 9; q++) {
+                              if (x == q) {
+                                  if (y == z) {
+                                      count++;
+                                  }
+                              }
+                          }
+                      }
+                  }
+                  }
+
               }
         return count;
     }
