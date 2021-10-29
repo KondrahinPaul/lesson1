d38d9d56dc8f466d75fac1ffeeded9788b2ea9d3
KondrahinPaul <sync.85@mail.ru>
2021-10-29 18:53:24 +0300

    clock

---
+++ src/main/java/com/ifmo/lesson2/SymmetricClocks.java
@@ -0,0 +1,31 @@
+package com.ifmo.lesson2;
+
+public class SymmetricClocks {
+    /*
+    Электронные часы показывают время в формате от 00:00 до 23:59. Подсчитать сколько
+    раз за сутки случается так, что слева от двоеточия показывается симметричная комбинация
+    для той, что справа от двоеточия (например, 02:20, 11:11 или 15:51)
+     */
+    public static void main(String[] args) {
+        System.out.println(symmetricTimes());
+    }
+
+    public static int symmetricTimes() {
+        // TODO implement
+        int count = 0;
+              for (int x = 0; x <= 1; x++) {
+                  for (int y = 0; y <= 9; y++) {
+for (int z = 0; z<=5; z++) {
+    for (int q=0;q<=9;q++) {
+        if (x==q) {
+            if (y==z) {
+                count++;
+            }
+        }
+    }
+}
+                                        }
+              }
+        return count;
+    }
+}
