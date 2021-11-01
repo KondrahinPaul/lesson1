91a968c8c42052a9d2629cc7e1a8aabeece618e9
KondrahinPaul <sync.85@mail.ru>
2021-11-01 15:02:55 +0300

    square

---
+++ src/main/java/com/ifmo/lesson2/SquareEquation.java
@@ -0,0 +1,37 @@
+package com.ifmo.lesson2;
+
+import java.util.Arrays;
+
+public class SquareEquation {
+    /*
+     В три переменные a, b и c записаны три вещественных числа. Создать программу, которая
+     будет находить и выводить на экран вещественные корни квадратного уравнения ax²+bx+c=0,
+     либо сообщать, что корней нет.
+     */
+    public static void main(String[] args) {
+        double a = 2;
+        double b = 9;
+        double c = 2;
+
+        double[] roots = squareEquationRoots(a, b, c);
+
+        System.out.println(Arrays.toString(roots));
+    }
+
+    /*
+    Возвращает массив из двух корней или null, если таковых нет.
+     */
+    public static double[] squareEquationRoots(double a, double b, double c) {
+        // TODO implement
+double[] ansve  = new double[2];
+
+        double d = 2*b-4*a*c;
+if (d>=0) {
+    ansve[0] = (-b-Math.sqrt(d))/2*a;
+    ansve[1] = (-b+Math.sqrt(d))/2*a;
+}else {
+    return null;
+}
+        return ansve;
+    }
+}
