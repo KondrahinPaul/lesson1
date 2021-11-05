9a725d56e5743977fddd06c3457ad9dc1127f59e
KondrahinPaul <sync.85@mail.ru>
2021-11-05 21:10:39 +0300

    isinterv

---
+++ src/main/java/com/ifmo/lesson2/IsInInterval.java
@@ -0,0 +1,39 @@
+package com.ifmo.lesson2;
+
+import java.util.Random;
+
+public class IsInInterval {
+    /*
+     Создать программу, которая будет проверять попало ли случайно выбранное из отрезка
+    [5;155] целое число в интервал (25;100) и сообщать результат на экран.Примеры работы
+    программы:
+    Число 113 не содержится в интервале (25,100) Число 72 содержится в интервале (25,100) 
+    Число 25 не содержится в интервале (25,100) Число 155 не содержится в интервале (25,100) 
+     */
+    public static void main(String[] args) {
+        int rnd = randomInt();
+        System.out.println(rnd);
+        String inInterval = isInInterval(rnd);
+
+        // TODO implement
+        System.out.println(inInterval);
+    }
+
+    public static int randomInt() {
+        // TODO implement
+        Random rnd = new Random();
+        int i = rnd.nextInt(150) + 5;
+        return i;
+    }
+
+    public static String isInInterval(int rnd) {
+        // TODO implement
+        if (rnd > 25) {
+            if (rnd < 100) {
+                return "Число " + rnd + " содержится в интервале (25,100)";
+            }
+        }
+        return "Число " + rnd + " не содержится в интервале (25,100)";
+    }
+
+}
