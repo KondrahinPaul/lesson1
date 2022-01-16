3608b99ff35969ea93b8e44b4ff3be555d5086ae
KondrahinPaul <sync.85@mail.ru>
2022-01-16 09:45:45 +0300

    from best

--- src/main/java/com/ifmo/Bank/Account.java
+++ src/main/java/com/ifmo/Bank/Account.java
@@ -1,42 +1,31 @@
 package com.ifmo.Bank;
 
-import java.util.Scanner;
-
 public class Account {
     private long id;
     private double amount;
     private long userId;
-    private boolean blocked = true;
-Scanner s = new Scanner(System.in);
-    public long getId() {
-        return id;
-    }
+    private boolean blocked;
 
-    public void setId(long id) {
-        this.id = id;
+    public Account(long userId){
+        this.userId = userId;
+        this.amount=amount;
+        this.blocked=blocked;
     }
-
-    public double getAmount() {
-        amount=s.nextDouble();
-        return amount;
-    }
-
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+    public double getAmount() {return amount;}
     public void setAmount(double amount) {
         this.amount = amount;
     }
-
     public long getUserId() {
         return userId;
     }
-
     public void setUserId(long userId) {
         this.userId = userId;
     }
-
     public boolean isBlocked() {
         return blocked;
     }
-
     public void setBlocked(boolean blocked) {
         this.blocked = blocked;
     }
--- src/main/java/com/ifmo/Bank/Bank.java
+++ src/main/java/com/ifmo/Bank/Bank.java
@@ -1,47 +1,45 @@
 package com.ifmo.Bank;
 
-import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-public class Bank implements Runnable {
+public class Bank{
 
-    private ConcurrentMap<long, Account> accMap = new ConcurrentMap<long, Account>() {
-        public Account createAccount(long userId) {
-            createAccount(long userId);
-            return null;
-        }
+    private ConcurrentMap<Long, Account> accMap = new ConcurrentHashMap<Long, Account>();
 
-        //        public doublegetAmount(long accId) {
-//
-//        }
-        public void blockAccount(long accId) {
-        }
-
-        public void transferMoney(long srcAccId, long dstAccId, double amount) {
-            srcAccId = 0;
-            Scanner scanner = new Scanner(System.in);
-            //for (; srcAccId<5; srcAccId++) {
-            amount = scanner.nextDouble();
-            accMap.put (srcAccId, Account);
-
-            //}
-
-
+    public Account createAccount(long userId) throws RuntimeException {
+        if (!accMap.containsKey(userId)) {
+            Account newAcc = new Account(userId);
+            accMap.put(userId, newAcc);
+            return newAcc;
+        } else {
+            throw new RuntimeException("account already exists");
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread();
-        Account a1 = new Account();
-
-        System.out.println(a1);
+    public void blockAccount(long accId) {
 
     }
 
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            accMap<>.put ();
+    public void transferMoney(long srcAccId, long dstAccId, double amount) throws RuntimeException {
+        Account srcContact = accMap.get(srcAccId);
+        Account dstContact = accMap.get(dstAccId);
+        if (srcContact != null && dstContact != null) {
+            if (srcContact.getAmount() - amount >= 0) {
+                synchronized (srcContact) {
+                    synchronized (dstContact) {
+                        srcContact.setAmount(srcContact.getAmount() - amount);
+                        dstContact.setAmount(dstContact.getAmount() + amount);
+                        System.out.println( srcAccId + " src Account have money: " + srcContact.getAmount());
+                        System.out.println(dstAccId + " dst Account have money: " + dstContact.getAmount());
+                    }
+                }
+            } else {
+                throw new RuntimeException("not enough money on srcAccount");
+            }
+        } else {
+            throw new RuntimeException("some Account is null");
         }
+
     }
 }
